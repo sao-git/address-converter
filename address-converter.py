9b8332d39e7a2390a5e72beceefb3abc0508356e
Sao Git <saogit@hotmail.com>
2018-10-20 15:30:14 -0500

    Modularization

--- address-converter.py
+++ address-converter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 ip token list/get returns a token in dot-decimal form.
 This script will convert a standard hex token to dot-decimal
@@ -9,8 +11,6 @@ Add conversion from dot-decimal to colon-hex
 Add exception handling for bad input strings
 """
 
-token = input("Hex token, e.g. dead:beef : ")
-token_p = (a.zfill(4) for a in token.split(':'))
 
 def recombine(t):
     s = ''
@@ -18,20 +18,24 @@ def recombine(t):
         s += a
     return s
 
-token_s = recombine(token_p)
 
 def split_by_eights(s):
     r = iter(s)
     return (a+b for a,b in zip(r,r))
     
-token_r = split_by_eights(token_s)
 
 def hex_to_int(t):
     return (int(b, 16) for b in t)
 
-token_d = hex_to_int(token_r)
 
 def int_to_dd(t):
     return '.'.join((str(x) for x in t))
 
-print(int_to_dd(token_d))
+
+if __name__ == "__main__":
+    token = input("Hex token, e.g. dead:beef : ")
+    token_p = (a.zfill(4) for a in token.split(':'))
+    token_s = recombine(token_p)
+    token_r = split_by_eights(token_s)
+    token_d = hex_to_int(token_r)
+    print(int_to_dd(token_d))
