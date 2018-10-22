3537e08173d17c6b8c36ba76eced8bdaf5a36d6c
Sao Git <saogit@hotmail.com>
2018-10-22 16:17:58 -0500

    Comments, cleanup, some skeletons.

--- exp_notation.py
+++ exp_notation.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 from math import log
 
-def sign(x):
-    # https://www.quora.com/How-do-I-get-sign-of-integer-in-Python
-    return x and (1, -1)[x < 0]
+"""
+TODO:
+    Define an exp_tuple type as output of said function with named indices?
+"""
 
-def exp_tuple(x, base = 10):
-    s, x = sign(x), abs(x)
-    a = int(log(x) / log(base))
-    return s * x / base**a, base, a
+def _normalize(x):
+    return
 
+def exp_tuple(x, base = 10, normalize = True):
+    # Real logarithms can't have negative arguments.
+    xa = abs(x)
+    # We only need the integer part of the logarithm.
+    # int() always truncates towards zero, so negative exponents are covered.
+    a = int(log(xa) / log(base))
+    return x / base**a, base, a
+
+def radix_base(t, base):
+    """
+    Convert exp_tuple to strings of `base` representation.
+    """
+    return
 
 if __name__ == "__main__":
     x = float(input("Number pls: "))
