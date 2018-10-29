bfc60c9d8a328df1cdfa34f9c9f4f67967713557
Sao Git <saogit@hotmail.com>
2018-10-29 18:59:24 -0500

    Feature adding and refactoring.

--- exp_notation.py
+++ exp_notation.py
@@ -22,13 +22,13 @@ def split_float(x):
     x_str = str(x)
 
     if x_str.count("e"):
-        x_s = x_str.split("e")
-        return float(x_s[0]), int(x_s[1])
+        x_s, x_p = x_str.split("e")
+        return float(x_s), int(x_p)
     else:
         return x, 0
 
 
-def normalize(x):
+def normalize(x, base = 10):
     """
     Converts a number to “normalized” or “standard” form, i.e. where
     there is exactly one decimal digit before the decimal point. This is the
@@ -42,9 +42,11 @@ def normalize(x):
 
     # Implementation detail:
     #
-    # Most cases use temporary strings, as this is more a problem of
-    # human readable representation rather than the underlying number.
-    # This choice makes the contents of the pair exact.
+    # This is the algorithm for perfect normalization which relies on
+    # string conversion to eliminate rounding error, but is very slightly
+    # slower than using normalize = True with exp_tuple() (~5.5 µs on a
+    # Pentium N3700 @ 1.60GHz) and currently only works for base
+    # and display_base of 10.
 
 
     # Nothing further needed if the whole part of |x| is in [1, 10).
@@ -52,25 +54,25 @@ def normalize(x):
     # when caught by the |x| < 1 case.
     x_abs = abs(x)
     if 1 <= x_abs < 10 or x == 0:
-        return x, 0
+        return x, 0, base
 
     elif x_abs < 1:
         # Dealing exclusively with floats in this case.
 
         # Check if x is in e notation. Since |x| < 1 is true, if the
         # output of split_float() has an exponent, nothing more is needed.
-        x_split = split_float(x)
-        if x_split[1] != 0:
-            return x_split
+        x_m, x_n = split_float(x)
+        if x_n != 0:
+            return x_m, x_n, base
 
 
         # Last case here is to make a temp string after the decimal point,
-        # count the number of zeroes until the first non-zero, and shift the decimal point
-        # to the right by that count plus one.
+        # count the number of zeroes until the first non-zero, and shift
+        #the decimal point to the right by that count plus one.
         else:
             post_dec = str(x_abs)[2:]
             shift = re.search("0*", post_dec).end() + 1
-            return sign(x) * float(post_dec[shift - 1] + "." + post_dec[shift:]), -1 * shift
+            return sign(x) * float(post_dec[shift - 1] + "." + post_dec[shift:]), -1 * shift, base
 
 
     # Final case is for x > 10. Count the length of the whole part of |x|,
@@ -84,40 +86,63 @@ def normalize(x):
             shift = len(str(int(x_abs))) - 1
         # If x is ±inf return x, x
         except OverflowError:
-            return x, x
+            return x, x, base
         #post_dec = str(x_abs).split('.')[1]
-        return x / 10**(shift), shift
+        return x / 10**(shift), shift, base
         #return sign(x) * float(post_dec[shift - 1] + "." + post_dec[shift:]), -1 * shift
 
 
-def exp_tuple(x, base = 10, normalize = False):
+def exp_tuple(x, base = 10, normalize = False, display_base = 10):
     """
-    For a number x and base b, return (m, b, n) where m = x / b^n, n = int(log_b(|x|))
+    For a number x and base b, return (m, n, b) where m = x / b^n, n = int(log_b(|x|))
+    If normalize is True, the output will be tuned to ensure 1 <= |m| display_base
 
     """
-    # (0, 0) will be the output for x = 0
+
+    # (0, 0, base) will be the output for x = 0
     if x == 0:
-        return 0, base, 0
-
-    # We only need the integral part of the logarithm.
-    # int() always truncates towards zero, so negative exponents are covered.
+        return 0, 0, base
     else:
-        n = int(math.log(abs(x), base))
+        x_log = math.log(abs(x), base)
+        # int() always truncates towards zero, so negative exponents are covered.
+        n = int(x_log)
+
+    # Conditional added here to mitigate rounding errors from exponentiation
+    # observed during testing.
+    m_calc = lambda x,n: x / pow(base, n) if n >= 1 else x * pow(base, -1 * n) 
+
+    if normalize == False:
         if n == 0:
-            return x, base, 0
-        # Conditional added here to mitigate rounding errors from exponentiation
-        # observed during testing.
-        elif n >= 1:
-            return x / pow(base, n), base, n
+            return x, 0, base
         else:
-            # Case n < 1
-            return x * pow(base, -1 * n), base, n
+            return m_calc(x,n), n, base
+    else:
+        t = x_log - n
+        log_b_B = math.log(display_base, base)
+
+    if 0 <= t < log_b_B:
+        return m_calc(x,n), n, base
+    else:
+        n = math.ceil(x_log - log_b_B)
+        return m_calc(x,n), n, base
 
 
-def radix_base(t, base):
+def radix_base(t):
     """
-    Convert exp_tuple to strings of `base` representation.
+    Make a new exp_tuple with m as a string of `base` representation, e.g.
+    if the base is 16, convert m to a hex string. This only works for conversions provided
+    by Python.
     """
+    m, n, b = t
+
+    #float.hex(), float.fromhex()
+    
+    m_i = int(m)
+    m_f = str(abs(m))[2:]
+    r = r"(0[xbo])"
+    m_i_r = re.search(r, m_i)
+    m_f_r = re.search(r, m_f)
+
     return
 
 if __name__ == "__main_":
@@ -127,11 +152,23 @@ if __name__ == "__main_":
 if __name__ == "__main__":
     x = float(input("Number pls: "))
     b_str = input("Base pls: ")
+    d = int(input("Display base pls: "))
     if b_str == "e":
         b = math.e
     elif b_str == "pi":
         b = math.pi
     else:
         b = int(b_str)
-    t = exp_tuple(x, b)
-    print(t)
+    print("\nUsing input: x = {}, b = {}, B = {}".format(x, b, d))
+    t = exp_tuple(x, b, normalize = True, display_base = d)
+    print("\nexp_tuple(x, base = {}, normalize = True, display_base = {:d}):\n        {}".format(b, d, t))
+    x_normal = normalize(x)
+    print("normalize(x): \n        " + str(x_normal))
+    x_recons = t[0] * t[2]**t[1]
+    print("\nReconstructed from exp_tuple: " + str(x_recons))
+    x_recons_n = x_normal[0] * 10**x_normal[1]
+    print("Reconstructed from normalize: " + str(x_recons_n))
+    print("Equal?: " + str(x_recons == x_recons_n))
+    x_comp = abs(x_recons - x_recons_n)
+    tolerance = 1e-20
+    print("Within {:.0e} of each other?: {}".format(tolerance, x_comp < tolerance))
