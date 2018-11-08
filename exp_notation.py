9884e35c7428cd2135c9ee35e73e09db928258f4
Sao Git <saogit@hotmail.com>
2018-11-07 19:33:06 -0600

    Commiting current changes before rewrite of hex_manip()

--- exp_notation.py
+++ exp_notation.py
@@ -6,6 +6,26 @@ import re
 #       Add a lookup table of SI prefixes, and convert from base 10
 #       to 1000 for engineering notation.
 
+# Valid values for display_base other than 10
+valid_radices = (2, 8, 16)
+# Functions for converting an integer to a string in a new base, in order of above
+int_convs = (bin, oct, hex)
+
+# Lookup table maps display_base as a key to conversion function as a value
+int_conv_funcs = dict(zip(valid_radices, int_convs))
+# Lookup table maps int as a key to int(log(int, 2)) as a value
+# Valid for powers of 2 from 0 to 1024
+radix_bit_conv = dict((2**n, n) for n in range(0, 11))
+
+
+class RadixError(Exception):
+    """Standard error message for a display_base not in valid_radices."""
+    def __init__(self):
+        radix_error = tuple(str(i) for i in sorted(valid_radices + (10,)))
+        radix_error = ", ".join(radix_error[:-1]) + ", or " + radix_error[-1]
+        radix_error = "Display base must be " + radix_error + "."
+        super(RadixError, self).__init__(radix_error)
+
 
 class exponential:
     si_prefixes = {
@@ -56,9 +76,17 @@ class exponential:
 
 def sign(x):
     """
-    Taken from https://www.quora.com/How-do-I-get-sign-of-integer-in-Python
+    Returns -1 or 1 based on the numeric sign of x.
+    Any number equivalent to False, including -0.0, will return itself.
     """
+
+    # From https://www.quora.com/How-do-I-get-sign-of-integer-in-Python
     return x and (1, -1)[x < 0]
+
+
+def sign_str(x):
+    return '-' if x < 0 else ''
+
 
 def split_float(x):
     """
@@ -158,11 +186,10 @@ def exp_tuple(x, base = 10, normalize = False):
         # form of |x|. Let b^t be called |m|, which is also |x|/b^n. Then x = x/b^n * b^n.
         #
         # This form is called "normalized" if 1 <= |m| < b, which means 0 <= t < 1, and
-        # occurs naturally for |x| >= 1. When 0 < |x| < b, 1 must be subtracted from the
-        # exponent n to make m fit this condition.
+        # occurs naturally for |x| >= 1.
         try:
-            int(x)
         # If x is ±inf or NaN return x, x, base
+            int(x)
         except (OverflowError, ValueError):
             return x, x, base
 
@@ -171,51 +198,106 @@ def exp_tuple(x, base = 10, normalize = False):
         n = int(x_log)
         t = x_log - n
 
-    # Conditional added here to mitigate rounding errors from exponentiation
-    # observed during testing.
-    m_calc = lambda x,n: x / pow(base, n) if n >= 1 else x * pow(base, -1 * n)
-
     if normalize and t < 0:
         # If 0 <= t < 1, do nothing. If t >= 0, it will be < 1 by definition.
         # If t < 0, subtract 1 from n, which implicitly adds 1 to t and guarantees
-        # it to be between 0 and 1. Subtracting 1 from n multiples m by b.
+        # it to be between 0 and 1. This has the effect of multiplying the original m by b.
         n -= 1
 
     if n == 0:
         # If 0 < |x| < base, n is zero, so m is just x.
         m = x
     else:
-        m = m_calc(x, n)
+        # Conditional added here to mitigate rounding errors observed during testing.
+        m = x / pow(base, n) if n >= 1 else x * pow(base, -n)
 
     return m, n, base
 
 
-def radix_base(t):
+def hex_manip(f, display_base = 16):
+    if display_base not in valid_radices:
+        raise RadixError
+    else:
+        f_t = float(f).hex()
+
+    # Capture the sign of f
+    sign_f = f_t[0] if f_t[0] != '0' else ''
+    # Determine how many places to skip the sign character and '0x'
+    skip = 3 if f < 0 else 2
+    # Mantissa is the absolute value, exponent is to base 2
+    mantissa, exponent = f_t[skip:].split('p')
+    exponent = int(exponent)
+    # Whole part is 1 for normal floats, 0 for subnormal
+    # Fraction part is in hex; each digit will be converted to int if display_base != 16
+    whole, fraction = mantissa.split('.')
+    #whole = int(whole)
+    whole = bin(whole)[2:].zfill(4)
+    print("{}.{}".format(whole,fraction))
+
+    #f_frac = tuple(int(x) / 16**i for i,x in enumerate(iter(f_m), 1))
+    if display_base != 16:
+        # Determine builtin function for conversion
+        int_conv_func = int_conv_funcs[display_base]
+        # Named generator for readibility
+        f_frac = tuple(int(x, 16) for x in iter(fraction))
+        f_frac_d = sum(x / 16**i for i,x in enumerate(f_frac, 1))
+        print("f_frac_d = " + str(f_frac_d))
+        # Convert digits to new base
+        #fraction = "".join(int_conv_func(x)[2:] for x in f_frac)
+        #fraction = tuple(int_conv_func(x)[2:] for x in f_frac)
+        fraction = "".join(bin(x)[2:].zfill(4) for x in f_frac)
+        #frac_d = sum(int(x, display_base) / display_base**i for i,x in enumerate(fraction, 1))
+        frac_d = sum(int(x, 2) / 2**i for i,x in enumerate(fraction, 1))
+        print("frac_d = " + str(frac_d))
+        print("f_frac_d == frac_d?: " + str(f_frac_d == frac_d))
+        shift = math.log(display_base, 2)
+        #frac_shifted = sign_f + whole 
+
+    return fraction
+
+
+def radix_base(t, display_base = 16, raw_hex = False):
     """
-    Make a new exp_tuple with each element as a string of base representation, e.g.
-    if the base is 16, convert m to a hex string. This is limited in scope to the "common"
-    bases, i.e. 2, 8, 10, 16, and 64.
+    Make a new exp_tuple with each element as a string of radix representation, e.g.
+    if display_base is 16, convert m to a hex string. This is limited in scope to the
+    "common" bases, i.e. 2, 8, 10, 16.
     """
-    m, n, b = t
 
-    #float.hex(), float.fromhex()
+    if raw_hex == True:
+        return tuple(float(_t).hex() for _t in t)
+    if display_base == 10:
+        return tuple(str(f) for f in t)
+    else:
+        m_t, n_t, b_t = t
 
-    m_i = int(m)
-    m_f = str(abs(m))[2:]
-    r = r"(0[xbo])"
-    m_i_r = re.search(r, m_i)
-    m_f_r = re.search(r, m_f)
+    int_conv_func = int_conv_funcs[display_base]
+    n = int_conv_func(n_t)
+    
+    if type(b_t) == int:
+        b = int_conv_func(b_t)
+    else:
+        None
 
-    return
+
+
+    #f.hex(), float.fromhex()
+
+
+    return m, n, b
+
 
 #if __name__ == "__main__":
 #    print(exp_tuple(float("-inf"), 10))
+
+if __name__ == "__main__":
+    x = float(input("Number pls: "))
+    print(hex_manip(x, 8))
 
 if __name__ == "__main_":
     a = float(input("Number pls: "))
     print(normalize(a))
 
-if __name__ == "__main__":
+if __name__ == "__main_":
     x = float(input("Number pls: "))
     b_str = input("Base pls: ")
     #d = int(input("Display base pls: "))
